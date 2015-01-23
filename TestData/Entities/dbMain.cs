c6568d7ddd0c19f230e64ccf213fb6022179cf2b
mgm <muratgumus05@gmail.com>
2015-01-23 14:12:59 +0200

    entity framework added with dbmain class

---
+++ TestData/Entities/dbMain.cs
@@ -0,0 +1,27 @@
+﻿namespace TestData.Entities
+{
+    public class DBMain
+    {
+        private static NorthwindEntities __db;
+
+        private static bool IsDbRefreshNeed { get; set; }
+
+        public static NorthwindEntities DB
+        {
+            get
+            {
+                if (IsDbRefreshNeed)
+                {
+                    RefreshDbAllEntity();
+                }
+                return __db ?? (__db = new NorthwindEntities());
+            }
+        }
+
+        public static void RefreshDbAllEntity()
+        {
+            __db = new NorthwindEntities();
+            IsDbRefreshNeed = false;
+        }
+    }
+}
